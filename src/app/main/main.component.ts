31c835a5b788e579b4893e20ffc1537e7f8a9a3a
MuhammadHassan199 <muhhassan199@gmail.com>
2023-08-25 16:35:38 +0500

    25-Aug-2023

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,3 +1,4 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent {
-
+  countries = [
+    { code: 'AE', name: 'United Arab Emirates' },
+    { code: 'IL', name: 'Israel' },
+    { code: 'JO', name: 'Jordan' },
+    { code: 'KW', name: 'Kuwait' },
+    { code: 'OM', name: 'Oman' },
+    { code: 'SA', name: 'Saudi Arabia' },
+    { code: 'PK', name: 'Pakistan' },
+    { code: 'VC', name: ' Saint Vincent And The Grenadines' },
+    { code: 'NL', name: 'Netherlands' },
+    { code: 'TD', name: 'Chad' },
+    { code: 'CD', name: ' Democratic Republic of the Congo' },
+    { code: 'EG', name: 'Egypt' },
+];
 }
